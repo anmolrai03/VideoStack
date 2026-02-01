51e68846257076d00df9374641f71adb8227fede
Anmol Rai <anmolrai0307@gmail.com>
2026-02-01 15:42:26 +0530

    added jwt services

---
+++ server/constants/jwt.js
@@ -0,0 +1,5 @@
+const JWT_ISSUER="videostack-api";
+const JWT_AUDIENCE="videostack-clients";
+const JWT_EXPIRES="1d";
+
+export {JWT_AUDIENCE , JWT_EXPIRES , JWT_ISSUER};
---
+++ server/services/jwt/jwtServices.js
@@ -0,0 +1,73 @@
+import jwt from "jsonwebtoken";
+
+import { JWT_AUDIENCE, JWT_ISSUER, JWT_EXPIRES } from "../../constants/jwt.js";
+import AppError from "../../utils/AppError.js";
+
+const JWT_SECRET = process.env.JWT_SECRET_KEY;
+
+if (!JWT_SECRET) {
+  throw new AppError({
+    statusCode: 500,
+    code: "MISSING_AUTH_KEY",
+    message: "Missing key for auth in jwt",
+  });
+}
+
+const jwtSign = (payload, id) => {
+
+  if( payload == null ) {
+    throw new AppError({
+      statusCode: 500,
+      code: "MISSING_PAYLOAD",
+      message: "missing data to be used to sign the token",
+    });
+  }
+
+  try {
+    const token = jwt.sign(payload, JWT_SECRET, {
+      subject: id.toString(),
+      issuer: JWT_ISSUER,
+      audience: JWT_AUDIENCE,
+      expiresIn: JWT_EXPIRES,
+      algorithm: "HS256",
+    });
+
+    return token;
+  } catch (error) {
+    console.log("[Error occured in jwtServices.]: ", error);
+    throw new AppError({
+      statusCode: 500,
+      code: "JWT_ERROR",
+      message: "Error in JWT token production."
+    })
+  }
+};
+
+const jwtVerify = (clientToken) => {
+  if( !clientToken ) {
+    throw new AppError({
+      statusCode: 401,
+      code: "CLIENT_TOKEN_MISSING",
+      message: 'Authentication token is required.'
+    })
+  }
+
+  try {
+    const decoded = jwt.verify(clientToken, JWT_SECRET, {
+      issuer: JWT_ISSUER,
+      audience: JWT_AUDIENCE,
+      algorithms: ["HS256"],
+    });
+
+    return decoded;
+  } catch (error) {
+    console.error("[Error in jwt verification]: ", error);
+    throw new AppError({
+      statusCode: 401,
+      code:"INVALID_TOKEN",
+      message: "Token verification error."
+    })
+  }
+};
+
+export { jwtSign, jwtVerify };
