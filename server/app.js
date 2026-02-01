441d51f271638a3a321ca04b6738dda2f26e8a01
Anmol Rai <anmolrai0307@gmail.com>
2026-02-01 15:54:25 +0530

    - updated cors setting - added cookie parser - added the errorHandler middleware

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
-// IMPORT MODULES
+// IMPORT MODULES STARTS HERE
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
-// IMPORT ROUTES
+// IMPORT ROUTES STARTS HERE
 import videoProcessingRoute from "./routes/videoProcessing.route.js"
+import authRoute from "./routes/auth.route.js"
+import errorHandler from './middlewares/errorhandler.middleware.js';
+import allowedOrigins from './constants/appCORS.js';
 
-// CONFIGURE DOTENV
+// CONFIGURE DOTENV STARTS HERE
 dotenv.config({
   path: './.env'
 });
@@ -18,7 +22,7 @@ const app = express();
 app.use(
   cors(
     {
-      origin: process.env.CORS_ORIGIN,
+      origin: allowedOrigins,
       credentials: true
     }
   )
@@ -30,12 +34,20 @@ app.use(express.json({limit: '16kb'}));
 // SET URL ENCODED LIMIT
 app.use(express.urlencoded({extended: true , limit: '16kb'}));
 
+// SET COOKIE PARSER
+app.use(cookieParser());
+
 // SET ROUTES
 app.use("/api", videoProcessingRoute);
+app.use("/api/auth", authRoute);
+
 
 // HEALTH CHECK ROUTE FOR PRODUCTION
 app.get('/health' , (req , res) => {
-  res.status(200).send("server active.")
+  res.status(200).send("server active.");
 });
+
+// ERROR MIDDLEWARE
+app.use(errorHandler);
 
 export default app;
