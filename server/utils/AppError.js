9ff6fb4e7d96dff67cf5405d32cefba9bd0112f9
Anmol Rai <anmolrai0307@gmail.com>
2026-02-01 15:52:50 +0530

    AppError has the errors field as well to throw consistent errors in the app

--- server/utils/AppError.js
+++ server/utils/AppError.js
@@ -1,8 +1,9 @@
 class AppError extends Error{
-  constructor({statusCode , code , message}){
+  constructor({statusCode , code , message, errors=[]}){
     super(message);
     this.statusCode = statusCode;
     this.code = code;
+    this.errors = errors;
 
     Error.captureStackTrace(this, this.constructor);
   }
