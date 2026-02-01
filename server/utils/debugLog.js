74111a8090abad22291f9c7f0b267a12d505d25e
Anmol Rai <anmolrai0307@gmail.com>
2026-02-01 16:24:19 +0530

    added util funciton for development debugLog

---
+++ server/utils/debugLog.js
@@ -0,0 +1,14 @@
+import { DEV_ENV } from "../constants/nodeEnv"
+
+/*
+  - USED FOR DEBUGGING 
+  - IF CURRENT ENVIRONMENT IS DEVELOPMENT
+  - SHOW THE CONSOLE LOGS ELSE DO NOTHING
+*/
+const debugLog = ( message, data) => {
+  if( process.env.NODE_ENV === DEV_ENV){
+    console.log(`[${message}: ${data}]`);
+  }
+};
+
+export default debugLog;
