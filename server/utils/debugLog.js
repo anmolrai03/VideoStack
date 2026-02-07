95433466740b62b86a52172a34bfb3b03d5f10e4
Anmol Rai <anmolrai0307@gmail.com>
2026-02-07 20:01:26 +0530

    updated debugLog for handling data = {}

--- server/utils/debugLog.js
+++ server/utils/debugLog.js
@@ -5,7 +5,7 @@ import { DEV_ENV } from "../constants/nodeEnv.js"
   - IF CURRENT ENVIRONMENT IS DEVELOPMENT
   - SHOW THE CONSOLE LOGS ELSE DO NOTHING
 */
-const debugLog = ( message, data) => {
+const debugLog = ( message, data={}) => {
   if( process.env.NODE_ENV === DEV_ENV){
     console.log(`[${message}: ${data}]`);
   }
