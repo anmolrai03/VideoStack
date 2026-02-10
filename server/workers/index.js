3f8871285a0e15c7876cc179e1639d02b125d073
Anmol Rai <anmolrai0307@gmail.com>
2026-02-10 14:05:45 +0530

    setup files for all workers

--- server/workers/index.js
+++ server/workers/index.js
@@ -1,5 +1,24 @@
 import "dotenv/config";
+import connectDb from "../configs/db.js";
+import getRedisConnection from "../configs/redis.js";
 
-import "./video.worker.js";
+async function startWorkers(){
+  try {
+    // connect mongodb
+    await connectDb();
 
-console.log("All Workers started running......")
+    //connect redis
+    const redisConnection = await getRedisConnection();
+    await redisConnection.ping();
+    // import workers
+    await import("./video.worker.js");
+
+    // message
+    console.log("All workers started.");
+  } catch (error) {
+    console.error("Workers start-up failed: ", error);
+    process.exit(1);
+  }
+}
+
+await startWorkers();
