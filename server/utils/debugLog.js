8fba42d2b32b12827490875b274da822d230fd04
Anmol Rai <anmolrai0307@gmail.com>
2026-02-10 14:06:23 +0530

    configured debugLog to handle null data

--- server/utils/debugLog.js
+++ server/utils/debugLog.js
@@ -5,9 +5,13 @@ import { DEV_ENV } from "../constants/nodeEnv.js"
   - IF CURRENT ENVIRONMENT IS DEVELOPMENT
   - SHOW THE CONSOLE LOGS ELSE DO NOTHING
 */
-const debugLog = ( message, data={}) => {
-  if( process.env.NODE_ENV === DEV_ENV){
-    console.log(`[${message}: ${data}]`);
+const debugLog = (message, data = null) => {
+  if (process.env.NODE_ENV === DEV_ENV) {
+    if (data) {
+      console.log(`[${message}]`, data ?? {});
+    } else {
+      console.log(`[${message}]`);
+    }
   }
 };
 
