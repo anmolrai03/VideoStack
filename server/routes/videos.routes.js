623a15525f6ec5b61068a30689cd92fe0c197a47
Anmol Rai <anmolrai0307@gmail.com>
2026-02-12 17:32:07 +0530

    added routes for getting homefeed video and get the url

--- server/routes/videos.routes.js
+++ server/routes/videos.routes.js
@@ -1,14 +1,24 @@
 import express from "express";
 import uploads from "../middlewares/multer.middleware.js";
 
-import { uploadVideoController , getUserVideosController, deleteVideoController, getVideoStatusController } from "../controller/videos.controller.js";
+import {
+  uploadVideoController,
+  getUserVideosController,
+  deleteVideoController,
+  getVideoStatusController,
+  getHomeFeedController,
+  getStreamUrl,
+} from "../controller/videos.controller.js";
 
 const router = express.Router();
 
-router.post("/upload",uploads.single("video"), uploadVideoController);
+router.post("/upload", uploads.single("video"), uploadVideoController);
+
+router.get("/", getHomeFeedController);
+router.get("/stream/:videoId", getStreamUrl);
+
 router.get("/user", getUserVideosController);
 router.delete("/user/delete/:videoId", deleteVideoController);
 router.get("/user/status/:videoId", getVideoStatusController);
-
 
 export default router;
