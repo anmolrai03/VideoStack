0db5f845290ef1c4ad6903e38117158cac94c3b5
Anmol Rai <anmolrai0307@gmail.com>
2026-02-19 18:36:28 +0530

    constants for auth and videos urls

---
+++ client/src/constants/APIConstants/authURLS.js
@@ -0,0 +1,9 @@
+const authURL = {
+  LOGIN_URL: "/api/auth/login",
+  REGISTER_URL: "/api/auth/register",
+  LOGOUT_URL: "/api/auth/logout",
+  VERIFY_PASSWORD_URL: "/api/auth/verify-password",
+  GET_ME_URL: "/api/auth/me"
+}
+
+export default authURL;
---
+++ client/src/constants/APIConstants/videosURLs.js
@@ -0,0 +1,10 @@
+const videoURL = {
+  UPLOAD_VIDEO_URL: "/api/videos/upload",
+  GET_ALL_VIDEOS_URL: "/api/videos/",
+  USER_VIDEOS_URL: "/api/videos/user",
+  STREAM_VIDEO_URL: "/api/videos/stream",
+  DELETE_VIDEO_URL: "/api/videos/user/delete",
+  GET_VIDEO_STATUS_URL: "/api/videos/user/status"
+}
+
+export default videoURL;
