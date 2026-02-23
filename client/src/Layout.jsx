9c5ce74e927601610c798f57239de0edf1258b00
Anmol Rai <anmolrai0307@gmail.com>
2026-02-24 00:24:00 +0530

    Layout done

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,12 +1,13 @@
-import React from "react";
 import Navbar from "./components/NavBar/NavBar";
 import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 
 function Layout() {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 }
