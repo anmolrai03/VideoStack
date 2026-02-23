73d6edba4a84274d4d4dae1f9a0bef41fb17f132
Anmol Rai <anmolrai0307@gmail.com>
2026-02-24 00:25:07 +0530

    added auth context provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import { Toaster } from "sonner";
 import RootRouter from "./router/RootRouter";
+import AuthContextProvider from "./contexts/AuthContext/AuthContextProvider"
 
 function App() {
   // console.log(import.meta.env.VITE_API_URL)
   return (
-    <>
+    <AuthContextProvider>
       <Toaster position="top-center" richColors="true" visibleToasts={3} />
       <RootRouter />
-    </>
+    </AuthContextProvider>
   );
 }
 
