4c6c65c5a3dbede9902d418d1c0a42650f6c74e5
Anmol Rai <anmolrai0307@gmail.com>
2026-03-05 23:22:25 +0530

    added auth routes

--- client/src/router/RootRouter.jsx
+++ client/src/router/RootRouter.jsx
@@ -1,15 +1,28 @@
 import {Route, RouterProvider , createBrowserRouter , createRoutesFromChildren} from 'react-router-dom';
 
+import Layout from '../Layout';
+
 import VideoProcess from '../pages/VideoProcess';
-import Home from '../pages/Home';
+import Home from '../pages/Home/Home';
+import Auth from '../pages/Auth/Auth';
+
+import Test from '../pages/Test';
 
 function RootRouter() {
 
   const router = createBrowserRouter(
     createRoutesFromChildren(
       <>
-        <Route path='/' element={ <Home /> } />
+        
         <Route path='/video/tools' element={<VideoProcess />} />
+        
+
+        <Route path='/' element={<Layout />}>
+          <Route index element={ <Home /> } />
+          <Route path='test' element={ <Test /> } />
+        </Route>
+
+        <Route path='/auth' element={<Auth />} />
       </>
     )
   )
