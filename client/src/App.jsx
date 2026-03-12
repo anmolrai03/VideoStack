44e5a73cd33ac5534438cc9f992c551475a69ff2
Anmol Rai <anmolrai0307@gmail.com>
2026-03-12 22:21:38 +0530

    customization for enablign dark theame in the toaster

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,24 @@
 import { Toaster } from "sonner";
 import RootRouter from "./router/RootRouter";
-import AuthContextProvider from "./contexts/AuthContext/AuthContextProvider"
+import AuthContextProvider from "./contexts/AuthContext/AuthContextProvider";
 
 function App() {
   // console.log(import.meta.env.VITE_API_URL)
   return (
     <AuthContextProvider>
-      <Toaster position="top-center" richColors="true" visibleToasts={3} />
+      <Toaster
+        position="top-center"
+        richColors="true"
+        visibleToasts={3}
+        theme="dark"
+        toastOptions={{
+          style: {
+            fontFamily: "Inter, system-ui, sans-serif",
+            fontWeight: 300,
+            letterSpacing: "-0.01em",
+          },
+        }}
+      />
       <RootRouter />
     </AuthContextProvider>
   );
