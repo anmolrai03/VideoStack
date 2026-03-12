d0a0f96897a7ef5714718dd6cc641489933f8483
Anmol Rai <anmolrai0307@gmail.com>
2026-03-12 22:42:30 +0530

    added suspense and fallback

--- client/src/router/RootRouter.jsx
+++ client/src/router/RootRouter.jsx
@@ -1,36 +1,71 @@
-import {Route, RouterProvider , createBrowserRouter , createRoutesFromChildren} from 'react-router-dom';
+import React, { lazy, Suspense } from "react";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromChildren,
+} from "react-router-dom";
 
-import Layout from '../Layout';
+import Layout from "../Layout";
 
-import VideoProcess from '../pages/VideoProcess';
-import Home from '../pages/Home/Home';
-import Auth from '../pages/Auth/Auth';
+// Lazy-loaded pages
+const Home = lazy(() => import("../pages/Home/Home"));
+const Auth = lazy(() => import("../pages/Auth/Auth"));
+const VideoProcess = lazy(() => import("../pages/VideoProcess"));
+const Test = lazy(() => import("../pages/Test"));
 
-import Test from '../pages/Test';
+// Loading component
+import Loading from "../pages/Loading/Loading";
 
 function RootRouter() {
-
   const router = createBrowserRouter(
     createRoutesFromChildren(
       <>
-        
-        <Route path='/video/tools' element={<VideoProcess />} />
-        
+        {/* UNPROTECTED ROUTES STARTS HERE */}
+        <Route
+          path="/video/tools"
+          element={
+            <Suspense fallback={<Loading />}>
+              <VideoProcess />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/auth"
+          element={
+            <Suspense fallback={<Loading />}>
+              <Auth />
+            </Suspense>
+          }
+        />
+        {/* UNPROTECTED ROUTES ENDS HERE */}
 
-        <Route path='/' element={<Layout />}>
-          <Route index element={ <Home /> } />
-          <Route path='test' element={ <Test /> } />
+        {/* PROTECTED ROUTES STARTS HERE */}
+        <Route path="/" element={<Layout />}>
+          <Route index
+            element={
+              <Suspense fallback={<Loading />}>
+                <Home />
+              </Suspense>
+            }
+          />
+          {/* Lazy-loaded test page */}
+          <Route
+            path="test"
+            element={
+              <Suspense fallback={<Loading />}>
+                <Test />
+              </Suspense>
+            }
+          />
         </Route>
+        {/* PROTECTED ROUTES ENDS HERE */}
 
-        <Route path='/auth' element={<Auth />} />
-      </>
-    )
-  )
+      </>,
+    ),
+  );
 
-
-  return(
-    <RouterProvider router={router}/>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default RootRouter
+export default RootRouter;
