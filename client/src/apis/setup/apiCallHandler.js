efe21cf1435bd6b067ad753c453320a663af7632
Anmol Rai <anmolrai0307@gmail.com>
2026-03-12 22:22:33 +0530

    added the errors from the server as data to be shown

--- client/src/apis/setup/apiCallHandler.js
+++ client/src/apis/setup/apiCallHandler.js
@@ -3,12 +3,18 @@ import { errorResponse, successResponse } from "./apiResponse";
 async function apiCallHandler(requestFn, fallbackSuccessMessage="Request Successful") {
   try {
     const response = await requestFn();
+    console.log("API CALL HANDLER(SUCCESS): ", response.data);
     return successResponse(
-      response?.data, response?.data?.message || fallbackSuccessMessage
+      response?.data?.data,
+      response?.data?.message || fallbackSuccessMessage
     )
   } catch (error) {
     console.log("API Error,",error.message,":", error);
-    return errorResponse(error?.response?.data?.message || error.message || "API Error Occured.")
+    console.log("API CALL HANDLER(ERROR): ", error?.response?.data)
+    return errorResponse(
+      error?.response?.data?.message || error.message || "API Error Occured.",
+      error?.response?.data?.errors
+    )
   }
 }
 
