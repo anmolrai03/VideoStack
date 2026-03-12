a687ab3d47953bcbec20c9eac9134ad770b78557
Anmol Rai <anmolrai0307@gmail.com>
2026-03-12 22:25:07 +0530

    input field component added from resuability

--- client/src/components/InputField/InputField.jsx
+++ client/src/components/InputField/InputField.jsx
@@ -5,8 +5,10 @@ function InputField({
   fieldName,
   showPassword = false,
   className = "",
+  error = "",
   ...rest
 }) {
+
   const type =
     inputType === "password"
       ? showPassword
@@ -15,10 +17,13 @@ function InputField({
       : inputType;
 
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 w-full">
+
       <label
         htmlFor={htmlFor}
-        className="text-xs tracking-wide text-white/60"
+        className={`text-xs tracking-wide transition ${
+          error ? "text-red-400" : "text-white/60"
+        }`}
       >
         {labelName}
       </label>
@@ -27,9 +32,28 @@ function InputField({
         id={htmlFor}
         name={fieldName}
         type={type}
-        className={`vs-input ${className}`}
+        aria-invalid={!!error}
+        aria-describedby={`${htmlFor}-error`}
+        className={`
+          vs-input
+          transition-all
+          ${error ? "border-red-500 focus:border-red-400" : ""}
+          ${className}
+        `}
         {...rest}
       />
+
+      <div className="min-h-4.5">
+        {error && (
+          <p
+            id={`${htmlFor}-error`}
+            className="text-base text-red-400 font-light tracking-wide"
+          >
+            {error}
+          </p>
+        )}
+      </div>
+
     </div>
   );
 }
