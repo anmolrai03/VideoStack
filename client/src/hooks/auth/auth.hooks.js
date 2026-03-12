627e0476978291c6b5a62cb2998103804fb52f29
Anmol Rai <anmolrai0307@gmail.com>
2026-03-12 22:23:22 +0530

    corrected useLogout hook to update the context's user data after logging out

--- client/src/hooks/auth/auth.hooks.js
+++ client/src/hooks/auth/auth.hooks.js
@@ -1,4 +1,7 @@
+import { useNavigate } from "react-router-dom";
+
 import {useAction} from "../generic/useAction.jsx";
+import { useAuthContext } from "../../contexts/AuthContext/AuthContext.js";
 
 import {getMeAPI, loginAPI, logoutAPI, registerAPI, verifyPasswordAPI} from "../../apis/auth/auth.api.js";
 
@@ -10,9 +13,23 @@ export function useRegister(){
   return useAction(registerAPI);
 }
 
-export function useLogout(){
-  return useAction(logoutAPI);
+export function useLogout() {
+  const { execute, loading } = useAction(logoutAPI);
+  const navigate = useNavigate();
+  const { refetchUser } = useAuthContext();
+
+  const logout = async () => {
+    const result = await execute();
+    if (result.success) {
+      await refetchUser(); // Re-fetch user to clear state
+      navigate("/auth");
+    }
+    return result;
+  };
+
+  return { logout, loading };
 }
+
 
 export function useVerifyPassword(){
   return useAction(verifyPasswordAPI);
